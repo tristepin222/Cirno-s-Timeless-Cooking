edee1e5ce779d101402f5eedc86abf82afe88686
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-09-02 12:02:39 +0200

    feat: changed receipes

--- Assets/scripts/Bowl.cs
+++ Assets/scripts/Bowl.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 public class Bowl : MonoBehaviour, IDropHandler
 {
     private int count = 0;
-    private Item[] items = new Item[26];
+    private Item[] items = new Item[31];
 
     private bool nothing = false;
     private bool containsFish = false;
@@ -28,6 +29,8 @@ public class Bowl : MonoBehaviour, IDropHandler
     private bool masterpieceForeign = false;
     private bool masterpieceYoukai = false;
     private bool badMeal = false;
+
+    private bool rawBeef, rocks, cookedMysteryplants, cookedMysteryBerries, cookedBeef, cookedRice, cookedFish, cookedLocallySourcedSparrow, cookedNoodles, ice, mysteryBox, cookedMagiciansMushrooms, bread, defrotstedFrogs, scarletDevilReserve, cookedEggs, mysteryBottle, kappaCucumbers, mysteryCan, mochi, cookedSweetPotatoes, mysterPowder, mysteryBerries = false;
     void IDropHandler.OnDrop(PointerEventData eventData)
     {
 
@@ -44,171 +47,213 @@ public class Bowl : MonoBehaviour, IDropHandler
     {
         foreach (Item item in items)
         {
-            if(item != null)
+            if (item == null)
             {
                 nothing = true;
             }
-            if(item.itemType == Item.ItemType.CookedFish)
+            else
             {
-                containsFish = true;
-            }
-            if (item.itemType == Item.ItemType.CookedEggs)
-            {
-                containsEgg = true;
-            }
-            if (item.itemType == Item.ItemType.DefrostedFrogs)
-            {
-                containsFrogs = true;
-            }
-            if (item.itemType == Item.ItemType.MagiciansMushrooms)
-            {
-                containsMagicMushrooms = true;
-            }
-            if (item.itemType == Item.ItemType.MysteryPlants)
-            {
-                containsMysterPlants = true;
-            }
-            if(item.itemType == Item.ItemType.RawBeef || item.itemType == Item.ItemType.Rocks || (item.itemType == Item.ItemType.MagiciansMushrooms && containsMagicMushrooms == false) || item.itemType == Item.ItemType.CookedMysteryPlants || item.itemType == Item.ItemType.CookedMysteryBerries || (item.itemType == Item.ItemType.MysteryPlants && containsMysterPlants == false) || item.itemType == Item.ItemType.MysteryBerries)
-            {
-                hasRaw = true;
-            }
-            if(item.itemType == Item.ItemType.CookedRice || item.itemType == Item.ItemType.CookedBeef || item.itemType == Item.ItemType.CookedFish || item.itemType == Item.ItemType.CookedLocallySourcedSparrow)
-            {
-                basicMealOnigiri = true;
-            }
-            if (item.itemType == Item.ItemType.CookedLocallySourcedSparrow || item.itemType == Item.ItemType.CookedBeef || item.itemType == Item.ItemType.Ice || item.itemType == Item.ItemType.CookedNoodles)
-            {
-                basicMealRamen = true;
-            }
-            if(item.itemType == Item.ItemType.RawEggs || item.itemType == Item.ItemType.Ice || item.itemType == Item.ItemType.MysteryBox)
-            {
-                goodMealCake = true;
-            }
-            if (item.itemType == Item.ItemType.RawEggs || item.itemType == Item.ItemType.Bread || item.itemType == Item.ItemType.CookedMagiciansMushrooms)
-            {
-                goodMealStuffedMagicianMushrooms = true;
-            }
-            if (item.itemType == Item.ItemType.DefrostedFrogs)
-            {
-                goodMealFrogLegs = true;
-            }
-            if (item.itemType == Item.ItemType.ScarletDevilReserve)
-            {
-                goodMealWine = true;
-            }
-            if (item.itemType == Item.ItemType.CookedEggs || item.itemType == Item.ItemType.CookedRice || item.itemType == Item.ItemType.CookedMagiciansMushrooms)
-            {
-                goodMealMushroomFriedRice = true;
-            }
-            if (item.itemType == Item.ItemType.CookedLocallySourcedSparrow || item.itemType == Item.ItemType.Bread || item.itemType == Item.ItemType.CookedEggs)
-            {
-                goodMealFriedChicken = true;
-            }
-            if (item.itemType == Item.ItemType.CookedRice || item.itemType == Item.ItemType.CookedMagiciansMushrooms || item.itemType == Item.ItemType.KappaCucumbers)
-            {
-                goodMealSimpleStirFry = true;
-            }
-            if(item.itemType == Item.ItemType.EarthRabbitMochi || item.itemType == Item.ItemType.MysteryBottle)
-            {
-                masterpieceMochi = true;
-            }
-            if (item.itemType == Item.ItemType.CookedBeef || item.itemType == Item.ItemType.CookedMinorikosSweetPotatoes || item.itemType == Item.ItemType.CookedMysteryPlants || item.itemType == Item.ItemType.MysteryPowder || item.itemType == Item.ItemType.ScarletDevilReserve)
-            {
-                masterpieceDevil = true;
-            }
-            if (item.itemType == Item.ItemType.CookedLocallySourcedSparrow || item.itemType == Item.ItemType.CookedMinorikosSweetPotatoes || item.itemType == Item.ItemType.MysteryCan || item.itemType == Item.ItemType.MysteryPowder)
-            {
-                masterpieceForeign = true;
-            }
-            if (item.itemType == Item.ItemType.CookedFish || item.itemType == Item.ItemType.KappaCucumbers || item.itemType == Item.ItemType.Ice || item.itemType == Item.ItemType.CookedNoodles || item.itemType == Item.ItemType.CookedEggs || item.itemType == Item.ItemType.MysteryPowder)
-            {
-                masterpieceYoukai = true;
-            }
-            if (!hasRaw && !basicMealOnigiri && !basicMealRamen && !goodMealCake && !goodMealStuffedMagicianMushrooms && !goodMealWine && !goodMealMushroomFriedRice && !goodMealFriedChicken && !goodMealSimpleStirFry && !masterpieceMochi && !masterpieceDevil && !masterpieceForeign && !masterpieceYoukai)
-            {
-                badMeal = true;
+                if (item.itemType == Item.ItemType.RawFish)
+                {
+                    containsFish = true;
+                }
+                if (item.itemType == Item.ItemType.RawEggs)
+                {
+                    containsEgg = true;
+                }
+                if (item.itemType == Item.ItemType.DefrostedFrogs)
+                {
+                    containsFrogs = true;
+                }
+                if (item.itemType == Item.ItemType.MagiciansMushrooms)
+                {
+                    containsMagicMushrooms = true;
+                }
+                if (item.itemType == Item.ItemType.MysteryPlants)
+                {
+                    containsMysterPlants = true;
+                }
+
+
+                if(item.itemType == Item.ItemType.RawBeef)
+                {
+                    rawBeef = true;
+                }
+                if(item.itemType == Item.ItemType.Rocks)
+                {
+                    rocks = true;
+                }
+                if (item.itemType == Item.ItemType.CookedMysteryPlants)
+                {
+                    cookedMysteryplants = true;
+                }
+                if (item.itemType == Item.ItemType.CookedMysteryBerries)
+                {
+                    cookedMysteryBerries = true;
+                }
+                if (item.itemType == Item.ItemType.MysteryBerries)
+                {
+                    mysteryBerries = true;
+                }
+
+                if(item.itemType == Item.ItemType.CookedRice)
+                {
+                    cookedRice = true;
+                }
+                if (item.itemType == Item.ItemType.CookedBeef)
+                {
+                    cookedBeef = true;
+                }
+                if (item.itemType == Item.ItemType.CookedFish)
+                {
+                    cookedFish = true;
+                }
+                if (item.itemType == Item.ItemType.CookedLocallySourcedSparrow)
+                {
+                    cookedLocallySourcedSparrow = true;
+                }
+                if (item.itemType == Item.ItemType.CookedNoodles)
+                {
+                    cookedNoodles = true;
+                }
+                if (item.itemType == Item.ItemType.Ice)
+                {
+                    ice = true;
+                }
+                if (item.itemType == Item.ItemType.MysteryBox)
+                {
+                    mysteryBox = true;
+                }
+                if (item.itemType == Item.ItemType.Bread)
+                {
+                    bread = true;
+                }
+                if (item.itemType == Item.ItemType.CookedMagiciansMushrooms)
+                {
+                    cookedMagiciansMushrooms = true;
+                }
+                if (item.itemType == Item.ItemType.DefrostedFrogs)
+                {
+                    defrotstedFrogs = true;
+                }
+                if (item.itemType == Item.ItemType.ScarletDevilReserve)
+                {
+                    scarletDevilReserve = true;
+                }
+                if (item.itemType == Item.ItemType.KappaCucumbers)
+                {
+                    kappaCucumbers = true;
+                }
+                if (item.itemType == Item.ItemType.MysteryBottle)
+                {
+                    mysteryBottle = true;
+                }
+                if (item.itemType == Item.ItemType.EarthRabbitMochi)
+                {
+                    mochi = true;
+                }
+                if (item.itemType == Item.ItemType.MysteryPowder)
+                {
+                    mysterPowder = true;
+                }
+                if (item.itemType == Item.ItemType.CookedMinorikosSweetPotatoes)
+                {
+                    cookedSweetPotatoes = true;
+                }
+                if (item.itemType == Item.ItemType.MysteryCan)
+                {
+                    mysteryCan = true;
+                }
+                if (item.itemType == Item.ItemType.CookedEggs)
+                {
+                    cookedEggs = true;
+                }
+
+                if (!hasRaw && !basicMealOnigiri && !basicMealRamen && !goodMealCake && !goodMealStuffedMagicianMushrooms && !goodMealWine && !goodMealMushroomFriedRice && !goodMealFriedChicken && !goodMealSimpleStirFry && !masterpieceMochi && !masterpieceDevil && !masterpieceForeign && !masterpieceYoukai)
+                {
+                    badMeal = true;
+                }
             }
         }
 
-        if(nothing && containsFish && containsEgg && containsFrogs && containsMagicMushrooms && containsMysterPlants)
+
+        if(containsFish && containsEgg && containsFrogs && containsMagicMushrooms && containsMysterPlants)
         {
             //trigger unique killer meal
             Debug.Log("unique killer meal");
         }
-        if (hasRaw)
+        if (cookedRice && (cookedBeef || cookedFish || cookedLocallySourcedSparrow))
         {
-            //trigger killer meal
+            //trigger basic meal
+            Debug.Log("basic onigiri meal");
+        }
+        if (cookedNoodles && ice && (cookedBeef || cookedLocallySourcedSparrow))
+        {
+            //trigger basic meal
+            Debug.Log("basic ramen meal");
+        }
+        if (containsEgg && ice && mysteryBox)
+        {
+            //trigger basic meal
+            Debug.Log("good cake meal");
+        }
+        if (containsEgg && bread && cookedMagiciansMushrooms)
+        {
+            //trigger basic meal
+            Debug.Log("good stuffed meal");
+        }
+        if (defrotstedFrogs)
+        {
+            //trigger basic meal
+            Debug.Log("good frog meal");
+        }
+        if (scarletDevilReserve)
+        {
+            //trigger basic meal
+            Debug.Log("good wine meal");
+        }
+        if (cookedEggs && cookedRice && cookedMagiciansMushrooms)
+        {
+            //trigger basic meal
+            Debug.Log("good fried rice meal");
+        }
+        if (cookedLocallySourcedSparrow && bread && cookedEggs)
+        {
+            //trigger basic meal
+            Debug.Log("good fried chicken meal");
+        }
+        if (cookedRice && cookedMagiciansMushrooms && kappaCucumbers)
+        {
+            //trigger basic meal
+            Debug.Log("good stir fry meal");
+        }
+        if (mochi && mysteryBottle)
+        {
+            //trigger basic meal
+            Debug.Log("masterpiece mochi meal");
+        }
+        if (cookedBeef && cookedSweetPotatoes && cookedMysteryplants && mysterPowder && scarletDevilReserve)
+        {
+            //trigger basic meal
+            Debug.Log("masterpiece devil meal");
+        }
+        if (cookedLocallySourcedSparrow && cookedSweetPotatoes && mysterPowder && mysteryCan)
+        {
+            //trigger basic meal
+            Debug.Log("masterpiece foreign meal");
+        }
+        if (cookedFish && kappaCucumbers && ice && cookedNoodles && cookedEggs && mysterPowder)
+        {
+            //trigger basic meal
+            Debug.Log("masterpiece youkai meal");
+        }
+        if (rawBeef || rocks || cookedMysteryplants || cookedMysteryBerries || containsMagicMushrooms || containsMysterPlants || mysteryBerries)
+        {
+            //trigger unique killer meal
             Debug.Log("killer meal");
         }
-        if (badMeal)
-        {
-            Debug.Log("bad meal");
-        }
-        if (basicMealOnigiri)
-        {
-            //trigger basic meal
-            Debug.Log("basic meal");
-        }
-        if (basicMealRamen)
-        {
-            //trigger basic meal
-            Debug.Log("basic meal");
-        }
-        if (goodMealCake)
-        {
-            //trigger basic meal
-            Debug.Log("good meal");
-        }
-        if (goodMealStuffedMagicianMushrooms)
-        {
-            //trigger basic meal
-            Debug.Log("good meal");
-        }
-        if (goodMealFrogLegs)
-        {
-            //trigger basic meal
-            Debug.Log("good meal");
-        }
-        if (goodMealWine)
-        {
-            //trigger basic meal
-            Debug.Log("good meal");
-        }
-        if (goodMealMushroomFriedRice)
-        {
-            //trigger basic meal
-            Debug.Log("good meal");
-        }
-        if (goodMealFriedChicken)
-        {
-            //trigger basic meal
-            Debug.Log("good meal");
-        }
-        if (goodMealSimpleStirFry)
-        {
-            //trigger basic meal
-            Debug.Log("good meal");
-        }
-        if (masterpieceMochi)
-        {
-            //trigger basic meal
-            Debug.Log("masterpiece meal");
-        }
-        if (masterpieceDevil)
-        {
-            //trigger basic meal
-            Debug.Log("masterpiece meal");
-        }
-        if (masterpieceForeign)
-        {
-            //trigger basic meal
-            Debug.Log("masterpiece meal");
-        }
-        if (masterpieceYoukai)
-        {
-            //trigger basic meal
-            Debug.Log("masterpiece meal");
-        }
-
+        Debug.Log("bad meal");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
 }
