2f84e7d77d9becf81a0e1eea7a4e04047973de19
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-09-02 14:42:09 +0200

    feat: added globalcontroller

---
+++ Assets/scripts/GlobalController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GlobalController : MonoBehaviour
+{
+    public static GlobalController Instance;
+    public float musicValue;
+    public float soundValue;
+    public bool hasLoadedScene;
+    public GameObject menu;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (SceneManager.GetActiveScene().name == "SampleScene")
+            {
+                Instance.menu.SetActive(true);
+            }
+        }
+    }
+}
