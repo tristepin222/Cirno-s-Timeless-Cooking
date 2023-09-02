230d44085011922077ab30c7504047a216a1963d
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-09-03 00:52:51 +0200

    Update Cooker.cs

--- Assets/scripts/Cooker.cs
+++ Assets/scripts/Cooker.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
+using UnityEngine.UI;
 public class Cooker : MonoBehaviour, IDropHandler
 {
     [SerializeField] List<Sprite> sprites = new List<Sprite>();
-
+    [SerializeField] Color toastedColor;
     void IDropHandler.OnDrop(PointerEventData eventData)
     {
         if(eventData.pointerDrag != null)
@@ -16,6 +16,7 @@ public class Cooker : MonoBehaviour, IDropHandler
             {
                 GameObject item = Instantiate(eventData.pointerDrag.gameObject, transform.parent);
                 item.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+                item.GetComponent<Image>().color = toastedColor;
                 dragger = item.GetComponent<Dragger>();
                 dragger.ResetState();
                 dragger.Cook();
